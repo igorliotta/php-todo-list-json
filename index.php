022bfe648de4636289be18a47edeb0afbc1b35ac
igorliotta <liottaigor@gmail.com>
2023-11-24 15:29:01 +0100

    Added: function fetchData() in app.js for recover data by my axios call to server.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link rel="stylesheet" href="./css/style.css">
     <title>PHP ToDo List JSON</title>
 </head>
@@ -13,7 +14,7 @@
             <h1>{{ title }}</h1>
             <section>
                 <ul>
-                    <li v-for="(todo, i) in todos" :key="i">{{ todo }}</li>
+                    <li v-for="(todo, i) in todos" :key="i">{{ todo.task }}</li>
                 </ul>
             </section>
         </div>
--- js/app.js
+++ js/app.js
@@ -4,7 +4,19 @@ const { createApp } = Vue
     data() {
       return {
         title: 'PHP ToDo List JSON',
-        todos: ['Fare la spesa','Pulire casa',"Chiamare l'idraulico"]
+        todos: [],
       }
+    },
+    methods: {
+        // Creo una funzione che mi permetta, tramite una chiamata AXIOS di tipo GET, di recuperarmi i dati e successivamente assegnarli all'array todos
+        fetchData() {
+            axios.get('server.php').then((res) => {
+                console.log(res.data)
+                this.todos = res.data
+            })
+        }
+    },
+    created() {
+        this.fetchData()
     }
   }).mount('#app')
